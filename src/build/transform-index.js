4cf755400fda8d1f99039fb222abe391ef13ab0d
LimeStreem <LimeStreem@gmail.com>
2016-09-25 18:20:05 +0900

    Add wire feature for generators

--- src/build/transform-index.js
+++ src/build/transform-index.js
@@ -2,7 +2,8 @@ import {
     readFileAsync,
     writeFileAsync,
     templateAsync,
-    globAsync
+    globAsync,
+    ensureFileAsync
 } from "../async-helper";
 import path from "path";
 const templateDir = path.join(__dirname, "../../src/build/templates/");
@@ -15,6 +16,44 @@ function getRelativePath(filePath) {
     const abs = path.resolve(filePath);
     const regex = /(.+)\.[^\.]+$/m;
     return "./" + path.relative(path.resolve('./src/'), abs).replace(regex, "$1");
+}
+
+const parseDependency = function*(text) {
+    let next = "";
+    let isComment = false;
+    for (let i = 0; i < text.length; i++) {
+        const c = text.charAt(i);
+        if (c === ";") {
+            isComment = true;
+            continue;
+        }
+        if (c === "\n") {
+            isComment = false;
+            if (!isComment && next.match(/.+/)) {
+                yield next;
+            }
+            next = "";
+            continue;
+        }
+        if (!isComment) {
+            next += c;
+        }
+    }
+    if (!isComment&& next.match(/.+/)) {
+        yield next;
+    }
+}
+
+async function parseDependencies() {
+    await ensureFileAsync(".grimoire.dependency");
+    const txt = await readFileAsync(".grimoire.dependency");
+    const dep = [];
+    for (let d of parseDependency(txt)) {
+        dep.push(d);
+    }
+    console.log(txt);
+    console.log(dep);
+    return dep;
 }
 
 
@@ -36,7 +75,7 @@ async function transform(config) {
         };
     });
     // glob converter files
-    const converterFiles = await globAsync(config.ts.src +'/**/*Converter.ts');
+    const converterFiles = await globAsync(config.ts.src + '/**/*Converter.ts');
     const converters = converterFiles.map(v => {
         const nameBody = getFileNameBody(v);
         const tag = nameBody.replace(/^(.+)Converter$/, "$1");
@@ -50,11 +89,11 @@ async function transform(config) {
         };
     });
     const imports = await templateAsync(templateDir + "imports.template", {
-        externals: config.dependencies,
+        externals: await parseDependencies(),
         components: components,
         converters: converters
     });
-    const register = await templateAsync(templateDir+"register.template", {
+    const register = await templateAsync(templateDir + "register.template", {
         namespace: config.namespace ? config.namespace : "HTTP://GRIMOIRE.GL/NS/CUSTOM",
         components: components,
         converters: converters
