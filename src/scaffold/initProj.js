65c1472c5d4ac81269ceb0a3fd1a76ba06867760
moajo <mimirosiasd@gmail.com>
2017-01-14 19:37:27 +0900

    implement init command

---
+++ src/scaffold/initProj.js
@@ -0,0 +1,25 @@
+import path from 'path';
+import fs from "fs";
+import unzip from "unzip";
+import { getSuffix } from "../buildUtil/common";
+
+import {
+  argv
+} from "yargs";
+
+function init() {
+  if (!argv.name) {
+    console.warn("name");
+    return;
+  }
+  const suffix = getSuffix(argv.name);
+  if (suffix) {
+    const cwd = process.cwd();
+    const p = path.join(__dirname, "../../boilerplate.zip");
+    fs.createReadStream(p).pipe(unzip.Extract({ path: cwd }));
+  } else {
+    console.warn("invalid name.")
+  }
+}
+
+init();
