7ded5f864a837b265297836490c86c0d9acc607b
moajo <mimirosiasd@gmail.com>
2017-01-15 02:06:19 +0900

    fix init command.

--- src/scaffold/initProj.js
+++ src/scaffold/initProj.js
@@ -2,17 +2,37 @@ import path from 'path';
 import fs from "fs";
 import unzip from "unzip";
 import { getSuffix } from "../buildUtil/common";
+import {
+  readFileAsync,
+  writeFileAsync
+} from "../async-helper";
 
 import {
   argv
 } from "yargs";
 
-function init() {
+async function init() {
   const suffix = getSuffix(argv.name);
   if (suffix) {
-    const cwd = process.cwd();
-    const p = path.join(__dirname, "../../boilerplate.zip");
-    fs.createReadStream(p).pipe(unzip.Extract({ path: cwd }));
+    try {
+      const cwd = process.cwd();
+      const p = path.join(__dirname, argv.ts ? "../../boilerplate.zip" : "../../boilerplate.zip"); //TODO:js boilerplate
+      const rs = fs.createReadStream(p);
+      const unzipExec = unzip.Extract({ path: cwd });
+      unzipExec.on('close', async function () {
+        try {
+          const packagePath = path.join(cwd, "/package.json");
+          let content = await readFileAsync(packagePath);
+          content = content.replace("<NAME>", argv.name);
+          writeFileAsync(packagePath, content)
+        } catch (e) {
+          console.error(e);
+        }
+      });
+      rs.pipe(unzipExec); //, { end: false }
+    } catch (e) {
+      console.error(e);
+    }
   } else {
     console.warn("invalid name.")
   }
