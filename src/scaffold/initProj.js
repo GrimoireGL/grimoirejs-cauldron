1c9f34df5b68e5e932211ff63cc64186dcef9e73
moajo <mimirosiasd@gmail.com>
2017-05-24 21:33:49 +0900

    update 'cauldron init'

--- src/scaffold/initProj.js
+++ src/scaffold/initProj.js
@@ -1,35 +1,23 @@
-import path from 'path';
-import fs from "fs";
-import unzip from "unzip";
+import simple_git from "simple-git";
+const git = simple_git()
 import { getSuffix } from "../buildUtil/common";
-import {
-  readFileAsync,
-  writeFileAsync
-} from "../async-helper";
+import { argv } from "yargs";
 
-import {
-  argv
-} from "yargs";
+const ts_boilerplate = "git@github.com:GrimoireGL/ts-boilerplate.git"
+const js_boilerplate = "git@github.com:GrimoireGL/js-boilerplate.git"
 
 async function init() {
   const suffix = getSuffix(argv.name);
   if (suffix) {
     try {
+      const bp = argv.js ? js_boilerplate : ts_boilerplate;
       const cwd = process.cwd();
-      const p = path.join(__dirname, argv.ts ? "../../boilerplate.zip" : "../../boilerplate.zip"); //TODO:js boilerplate
-      const rs = fs.createReadStream(p);
-      const unzipExec = unzip.Extract({ path: cwd });
-      unzipExec.on('close', async function () {
-        try {
-          const packagePath = path.join(cwd, "/package.json");
-          let content = await readFileAsync(packagePath);
-          content = content.replace("<NAME>", argv.name);
-          writeFileAsync(packagePath, content)
-        } catch (e) {
+      console.log(`cloning boilerplate from ${bp} ...`);
+      git.clone(bp, cwd).removeRemote("origin", function (e) {
+        if (e) {
           console.error(e);
         }
       });
-      rs.pipe(unzipExec); //, { end: false }
     } catch (e) {
       console.error(e);
     }
