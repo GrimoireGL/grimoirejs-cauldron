e59feb2aad317c3d395ee416eb3088e7280bd6e7
moajo <mimirosiasd@gmail.com>
2017-05-27 23:01:30 +0900

    fix: name field of package.json is rewritten with the specified name

--- src/scaffold/initProj.js
+++ src/scaffold/initProj.js
@@ -1,7 +1,9 @@
 import simple_git from "simple-git";
+import path from 'path';
 const git = simple_git()
 import { getSuffix } from "../buildUtil/common";
 import { argv } from "yargs";
+import { readFileAsync, writeFileAsync } from "../async-helper";
 
 const ts_boilerplate = "git@github.com:GrimoireGL/ts-boilerplate.git"
 const js_boilerplate = "git@github.com:GrimoireGL/js-boilerplate.git"
@@ -13,10 +15,13 @@ async function init() {
       const bp = argv.js ? js_boilerplate : ts_boilerplate;
       const cwd = process.cwd();
       console.log(`cloning boilerplate from ${bp} ...`);
-      git.clone(bp, cwd).removeRemote("origin", function (e) {
+      git.clone(bp, cwd).removeRemote("origin", async function (e) {
         if (e) {
           console.error(e);
         }
+        let package_json = await readFileAsync(path.join(cwd, "package.json"));
+        writeFileAsync(path.join(cwd, "package.json"), package_json.replace('"name": "grimoirejs-ts-boilerplate"', `"name": "grimoire-${suffix}"`));
+
       });
     } catch (e) {
       console.error(e);
