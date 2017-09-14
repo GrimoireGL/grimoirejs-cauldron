eab37549f772ffa9c2550cc367d3dd4574e9e38f
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-09-14 10:45:35 +0900

    fix: fix test

--- test/test.sh
+++ test/test.sh
@@ -3,5 +3,6 @@ cd ./temp
 ../bin/cauldron init --name "grimoirejs-test"
 ../bin/cauldron scaffold -t "component" -n "TestComponent"
 ../bin/cauldron scaffold -t "converter" -n "TestConverter"
+sed -i -e "s/SOMETHING/parseFloat/g" ./src/Converter/TestConverter.ts
 npm install
 npm run build -- --env.prod
